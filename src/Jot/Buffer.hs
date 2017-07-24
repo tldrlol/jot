cfbb595bc73cd5195d640b1cbc1a41626c39a508
Daniel Kirillov <tldrlol@protonmail.com>
2017-07-24 23:42:57 +0300

    Calculate newlines separately on file load

--- src/Jot/Buffer.hs
+++ src/Jot/Buffer.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Jot.Buffer
   ( Buffer(..)
   , load
   ) where
 
 import           Control.Concurrent.Async
-import qualified Data.ByteString          as BS
+import qualified Data.ByteString.Lazy     as BL
 
 import qualified Jot.Edit    as E
 import qualified Jot.History as H
@@ -14,11 +16,21 @@ import qualified Jot.Rope    as S
 -- |
 newtype Buffer = Buffer
   { edits :: H.History (R.Range, S.JotString)
-  }
+  } deriving (Show)
+
+-- |
+forceAsync :: a -> IO (Async a)
+forceAsync a = do
+  async $ do
+    pure ()
+    let !a' = a
+    pure a'
 
 -- |
 load :: FilePath -> IO (Async Buffer)
-load
-  = async
-  . fmap (Buffer . E.load . S.singleton)
-  . BS.readFile
+load p = async $ do
+  f <- BL.readFile p
+  let  s = S.concat $ S.singleton <$> BL.toChunks f
+  let !_ = S.length s
+  _ <- forceAsync (S.newLines s)
+  pure $ Buffer $ E.load s
